06bb7b5405c9165f5a510ac88709508b73fad180
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-03-14 16:14:55 -0300

    Gallery Section finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Feedbacks from "../components/Feedbacks/Feedbacks";
+import GallerySection from "../components/ImageSection/GallerySection";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <GallerySection />
       <Feedbacks />
       <Footer />
     </>
