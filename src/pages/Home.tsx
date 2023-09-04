9799da83e71261452fcf920a356a7b42a9859a42
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-09-04 16:50:08 -0300

    next js aplication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,19 @@
-import Feedbacks from "../components/Feedbacks/Feedbacks";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
-import Card from "../components/Section/Card";
-import Hero from "../components/Hero/Hero";
-import GallerySection from "../components/ImageSection/GallerySection";
+import Feedbacks from '../components/Feedbacks/Feedbacks';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Card from '../components/Section/Card';
+import Hero from '../components/Hero/Hero';
+import GallerySection from '../components/ImageSection/GallerySection';
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <GallerySection />
-      <Card />
-      <Feedbacks />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Hero />
+			<GallerySection />
+			<Card />
+			<Feedbacks />
+			<Footer />
+		</>
+	);
 }
